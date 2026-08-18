ee3d36088af1207c267035576167df75b0ebad32
doctorrayjay117 <arminhappy2@gmail.com>
2026-08-18 17:41:36 -0400

    Implemented a scroll-to-top button that activates once the user has scrolled to the halfway point of the page, beginning updating placeholders with actual band members

--- javascript.js
+++ javascript.js
@@ -6,3 +6,19 @@ function updateCopyrightYear() {
 const copyright = document.querySelector(".copyright");
 copyright.textContent = "© " + updateCopyrightYear().toString() + " The Leightones. All rights reserved.";
 
+const buttonScrollTop = document.querySelector(".scroll-to-top");
+
+buttonScrollTop.addEventListener("click", () => {
+    window.scrollTo({top: 0, behavior: "smooth"});
+});
+
+
+document.addEventListener("scroll", () => {
+    const currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+    const halfwayPoint = window.innerHeight * 0.5;
+    if(scrollDiff > halfwayPoint) {
+        buttonScrollTop.style.display = "block";
+    } else {
+        buttonScrollTop.style.display = "none";
+    }
+});
